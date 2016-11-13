9f6fa4cc5d36d247405601164464558cecd660ee
Paweł Jarosz <buurzuum@gmail.com>
2016-11-13 17:44:35 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -15,15 +15,4 @@ $('.parallax-window').parallax({ imageSrc: './images/img_3.jpg' });
     });
 }());
 
-
-// jQuery scroll to
-(function() {
-    $('li a[href*=#]').bind("click", function(e) {
-        var anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $(anchor.attr('href')).offset().top - 79
-        }, 1000);
-        e.preventDefault();
-    });
-}());
 });
